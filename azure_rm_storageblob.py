d2bf9b073ca1cf62bb86e2523137fae8a2cefc1e
chouseknecht <chouseknecht@ansible.com>
2016-01-24 01:03:12 -0500

    Fixed copyright.

--- azure_rm_storageblob.py
+++ azure_rm_storageblob.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# (c) 2015 Chris Houseknecht, <chouse@ansible.com>
+# (c) 2016 Chris Houseknecht, <chouseknecht@ansible.com>
 #
 # This file is part of Ansible
 #
